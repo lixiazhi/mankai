a55c0582474eae0104d0039a2ca17aa56844611d
summer <1016970809@qq.com>
2019-05-17 17:21:50 +0800

    0517页面添加js功能

--- js/index.js
+++ js/index.js
@@ -15,7 +15,6 @@ $(function () {
       $li.each(function (item, arr) {
         // console.log(item, arr);
         total += $(this).width()
-
       })
       // 每个li 有左右10px的padding
       total += liCount * 20
@@ -34,7 +33,6 @@ $(function () {
   })
   // 页面一加载先触发一次resize
   $(window).trigger('resize')
-
 })
 /* 
  资质荣誉点击修改active样式
@@ -60,12 +58,11 @@ $(function () {
   // 路由标识
   // 页面获取存储在sessionStorage中
   // 刷新页面时先从sessionStorage中获取
-  const contentTabUrl = JSON.parse(sessionStorage.getItem('contentTabUrl'))
-  console.log(contentTabUrl);
+  const contentTabUrl = sessionStorage.getItem('contentTabUrl')
   if (contentTabUrl) {
     $('.mk_content .nav-wrapper>ul>li').each(function (index, item) {
-      $(item).removeClass('active')
-      if ($(this).data('contenttaburl') === contentTabUrl) {
+      $(this).removeClass('active')
+      if ($(this).data('contenttaburl') == contentTabUrl) {
         $(this).addClass('active')
         // 对应右侧tab添加active
         const activeA = $(this).find('a').attr('href')
@@ -74,12 +71,17 @@ $(function () {
       }
     })
   }
+  // 点击tan栏时存储sessionStorage
   $('.mk_content .nav-wrapper>ul>li').on('click', function () {
-    sessionStorage.setItem('contentTabUrl', JSON.stringify($(this).data('contenttaburl')))
+    sessionStorage.setItem('contentTabUrl', $(this).data('contenttaburl'))
   })
   // 点击时存储id
-  $('.tab-content .qualification>ul>li').find('a').on('click', function () {
-    $('.Honor').toggleClass('hidden')
-    $(`.${$(this).data('url')}`).toggleClass('hidden')
-  })
+  $('.tab-content .qualification>ul>li')
+    .find('a')
+    .on('click', function () {
+      console.log($(this).attr('href'));
+
+      $('.Honor').toggleClass('hidden')
+      $(`.${$(this).data('url')}`).toggleClass('hidden')
+    })
 })
